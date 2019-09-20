ed96aff340f3081ea37ee3cf52c65a8d13c44647
bellakratchei <53630203+bellakratchei@users.noreply.github.com>
2019-09-20 17:40:08 -0300

    Update get.php

--- get.php
+++ get.php
@@ -5,10 +5,11 @@
 <body>
     <div>
         <?php
-            $nome=$_GET["nome"];
-            $ano=$_GET["ano"];
-            $idade=date("Y")-$ano;
-            echo "$nome tem $idade anos";
+            //$nome=$_GET["nome"];
+            //$ano=$_GET["ano"];
+            //$idade=date("Y")-$ano;
+            //echo "$nome tem $idade anos";
+            //var_dump($_GET);
         ?>
         <a href="get.html">Voltar</a>
     </div>
